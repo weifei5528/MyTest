97acfb33e822785aacf71a01746e7420f77a0185
weifei <weifei528@163.com>
2018-08-27 23:41:44 +0800

    api测试添加

--- application/common/service/Base.php
+++ application/common/service/Base.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\common\service;
+use GuzzleHttp\Client;
+
 /**
  * Created by PhpStorm.
  * User: Administrator
@@ -9,5 +11,45 @@ namespace app\common\service;
 abstract class Base
 {
     protected $token;
-    
+    protected $client;
+    protected $pageSize=10;
+    protected $httpsOption = false;
+    public function __construct()
+    {
+        $this->client = new Client(['verify' => $this->httpsOption]);
+
+    }
+
+    /**
+     * 获取授权的token
+     * @return mixed
+     *
+     */
+    public abstract function auth();
+
+    /**
+     * 获取公共的图片 不需要授权
+     * @return mixed
+     */
+    public abstract function getPublicImageList($page=1);
+
+    /**
+     * 获取
+     */
+    public abstract function getMyImageList();
+    /**
+     * 设置每页的数量
+     */
+    public function setPageSize($pageSize)
+    {
+        $this->pageSize = $pageSize;
+        return $this;
+    }
+    /**
+     * 设置 https验证
+     */
+    public function setHttpsOption($default = false)
+    {
+        $this->client->setDefaultOption('verify', $default);
+    }
 }
