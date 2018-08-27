737d85fd31ca3c57f6d7ca1c2bb57e183124471f
weifei <weifei528@163.com>
2018-08-27 21:44:17 +0800

    插件

---
+++ application/common/service/Base.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\common\service;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/27
+ * Time: 21:42
+ */
+abstract class Base
+{
+    protected $token;
+    
+}
